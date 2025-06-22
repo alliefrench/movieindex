fe85331c3d493e46c0d5a3ef9b2d742af523803c
Allie French <41064675+alliefrench@users.noreply.github.com>
2025-06-22 19:36:34 -0400

    Update index.tsx

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-function App() {
+function App(): JSX.Element {
   return <h1>Welcome to movidex app</h1>;
 }
 
 const container = document.getElementById("root");
+if (!container) {
+  throw new Error("Root container not found");
+}
 const root = createRoot(container);
 root.render(<App />);
