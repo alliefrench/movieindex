363190c9660d93499e51bc96106c785860688d78
Allie French <afrench@seatgeek.com>
2025-07-08 20:54:17 -0400

    check url

--- api/database.py
+++ api/database.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -67,4 +68,5 @@ async def get_db():
         try:
             yield session
         finally:
-            await session.close() 
+            await session.close()
+            logging.warning("Database session closed") 
